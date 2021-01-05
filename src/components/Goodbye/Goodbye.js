7e2fe48b6feb0cec52aff5d6a861e7b70dc2f2a1
Lucien <ariel.oliva13@gmail.com>
2021-01-05 18:53:40 -0300

    começo aula de react

---
+++ src/components/Goodbye/Goodbye.js
@@ -0,0 +1,12 @@
+import './Goodbye.css';
+import { Component } from 'react';
+
+export class Goodbye extends Component {
+    render() {
+        return (
+            <div className="goodbye">
+                <p>you say goodbye</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,9 @@
+import './Hello.css';
+
+export const Hello = () => {
+    return (
+        <div className="hello">
+            <p>Hello hello </p>
+        </div>
+    )
+} ; 
